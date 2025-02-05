0e84665dafe79a47b0e67d96b7d1d643ae06608d
Rahul Kumar <rks950408@gmail.com>
2025-02-04 18:50:27 -1100

    push

--- Basic Level Programming/3.py
+++ Basic Level Programming/3.py
@@ -1,3 +1,8 @@
 # 3. Write a Python program to check if a string is a palindrome.
 # 	Description: A palindrome is a word, phrase, or sequence that reads the same forward and backward.
 # 	Hint: Compare the string with its reverse (s == s[::-1]).
+
+num1=int(input("Enter the 1st number :"))
+
+num2=int(input("Enter the 2nd number :"))
+
